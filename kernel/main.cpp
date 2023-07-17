39687e52d6f502d9dd0e945e57e8833c8588d568
t-ngtn <obutaiei@gmail.com>
2023-07-17 18:10:02 +0000

    done day5e

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <cstdio>
 
+#include "console.hpp"
 #include "font.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
@@ -27,25 +28,17 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
 
   for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
     for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      pixel_writer->Write(x, y, {255, 255, 255});
-    }
-  }
-  for (int x = 0; x < 200; ++x) {
-    for (int y = 0; y < 100; ++y) {
-      pixel_writer->Write(x, y, {0, 255, 0});
+      pixel_writer->Write(x, y, {0, 0, 0});
     }
   }
 
-  int i = 0;
-  for (char c = '!'; c <= '~'; ++c, ++i) {
-    WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
-  }
-
-  WriteString(*pixel_writer, 0, 66, "Hello, ngtn world!", {0, 0, 255});
+  Console console{*pixel_writer, {0, 255, 0}, {0, 0, 0}};
 
   char buf[128];
-  sprintf(buf, "1 + 2 = %d", 1 + 2);
-  WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
+  for (int i = 0; i < 27; ++i) {
+    sprintf(buf, "line %d\n", i);
+    console.PutString(buf);
+  }
 
   while (1) __asm__("hlt");
 }
