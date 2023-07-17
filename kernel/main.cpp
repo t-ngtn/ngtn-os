56b8996debe15e3ea2d663419f83dde100ad51d8
t-ngtn <obutaiei@gmail.com>
2023-07-17 13:33:43 +0000

    done day5a

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,11 +3,29 @@
 
 #include "frame_buffer_config.hpp"
 
+const uint8_t kFontA[16] = {
+    0b00000000,  //
+    0b00011000,  //    **
+    0b00011000,  //    **
+    0b00011000,  //    **
+    0b00011000,  //    **
+    0b00100100,  //   *  *
+    0b00100100,  //   *  *
+    0b00100100,  //   *  *
+    0b00100100,  //   *  *
+    0b01111110,  //  ******
+    0b01000010,  //  *    *
+    0b01000010,  //  *    *
+    0b01000010,  //  *    *
+    0b11100111,  // ***  ***
+    0b00000000,  //
+    0b00000000,  //
+};
+
 struct PixelColor {
   uint8_t r, g, b;
 };
 
-// #@@range_begin(pixel_writer)
 class PixelWriter {
  public:
   PixelWriter(const FrameBufferConfig& config) : config_{config} {}
@@ -22,9 +40,7 @@ class PixelWriter {
  private:
   const FrameBufferConfig& config_;
 };
-// #@@range_end(pixel_writer)
 
-// #@@range_begin(derived_pixel_writer)
 class RGBResv8BitPerColorPixelWriter : public PixelWriter {
  public:
   using PixelWriter::PixelWriter;
@@ -48,18 +64,28 @@ class BGRResv8BitPerColorPixelWriter : public PixelWriter {
     p[2] = c.r;
   }
 };
-// #@@range_end(derived_pixel_writer)
 
-// #@@range_begin(placement_new)
+void WriteAscii(PixelWriter& writer, int x, int y, char c,
+                const PixelColor& color) {
+  if (c != 'A') {
+    return;
+  }
+  for (int dy = 0; dy < 16; ++dy) {
+    for (int dx = 0; dx < 8; ++dx) {
+      if ((kFontA[dy] << dx) & 0x80u) {
+        writer.Write(x + dx, y + dy, color);
+      }
+    }
+  }
+}
+
 void* operator new(size_t size, void* buf) { return buf; }
 
 void operator delete(void* obj) noexcept {}
-// #@@range_end(placement_new)
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
-// #@@range_begin(call_pixel_writer)
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
@@ -82,6 +108,9 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
+
+  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+  WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+
   while (1) __asm__("hlt");
 }
-// #@@range_end(call_pixel_writer)
