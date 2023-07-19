51c72917c90c1f01c58066a28ab4239c2110ee85
t-ngtn <obutaiei@gmail.com>
2023-07-19 13:06:37 +0000

    fix DrawRectangle

--- kernel/graphics.cpp
+++ kernel/graphics.cpp
@@ -18,7 +18,7 @@ void DrawRectangle(PixelWriter& writer, const Vector2D<int>& pos,
                    const Vector2D<int>& size, const PixelColor& c) {
   for (int dx = 0; dx < size.x; ++dx) {
     writer.Write(pos.x + dx, pos.y, c);
-    writer.Write(pos.x + dx, pos.y, c);
+    writer.Write(pos.x + dx, pos.y + size.y - 1, c);
   }
   for (int dy = 1; dy < size.y - 1; ++dy) {
     writer.Write(pos.x, pos.y + dy, c);
